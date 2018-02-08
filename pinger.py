84056862528748d63b046e6ea2b7fb4c435784c3
Onkar Dhane <engg.onkar@gmail.com>
2018-02-08 11:04:41 +0530

    Create pinger.py

---
+++ pinger.py
@@ -0,0 +1,16 @@
+import sys
+import datetime
+import os
+def ping(address):
+    if((os.system('ping %s -n 1' % (address,)))==0):
+        with open('result.txt','a') as file:
+            file.write("\nHost %s is up" %(address))
+    else:
+        with open('result.txt','a') as file:
+            file.write("\nHost %s is down"%(address))
+
+with open('result.txt','a') as res:
+    res.write(str(datetime.datetime.now()))
+with open("list.txt") as f:
+    for line in f:
+        ping(line)
